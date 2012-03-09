d0f5d797cc36413b14c01d5def2ec2a0f9f7e7b3
Ron Savage <ron@savage.net.au>
2012-03-09 06:13:50 +0000

    Import of RSAVAGE/Set-Array-0.25 from CPAN.
    gitpan-cpan-distribution: Set-Array
    gitpan-cpan-version:      0.25
    gitpan-cpan-path:         RSAVAGE/Set-Array-0.25.tar.gz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/Set/Array.pm
+++ lib/Set/Array.pm
@@ -26,7 +26,7 @@ use overload
    ">>=" => "pop",
    "fallback" => 1;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 sub new{
    my($class,@array) = @_;
@@ -1108,19 +1108,19 @@ If an error is thrown, I just ignore it. This is horribly tacky, but after waiti
 Sample code:
 
 	#!/usr/bin/env perl
-	
+
 	use strict;
 	use warnings;
-	
+
 	use Set::Array;
-	
+
 	# -------------
-	
+
 	my($set1) = Set::Array->new(qw(abc def ghi jkl mno));
 	my($set2) = Set::Array->new(qw(def jkl pqr));
 	my($set3) = $set1 - $set2;
 	my($set4) = Set::Array -> new(@{$set1 - $set2});
-	
+
 	print '1: ', join(', ', @$set3), ". \n";
 	print '2: ', join(', ', @{$set4 -> print}), ". \n";
 	print '3: ', join(', ', $set4 -> print), ". \n";
